b71fdc702b5a4215f51516607e40904c787e744c
nd199 <naren06251999@gmail.com>
2023-09-04 13:51:30 +0530

    moved dc.yaml out of backend

--- backend/src/main/java/com/codenaren/hashtag/HashtagApplication.java
+++ backend/src/main/java/com/codenaren/hashtag/HashtagApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class HashtagApplication {
+public class  HashtagApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(
